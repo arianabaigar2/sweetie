dc89bcb6e6cfe5ad4e9371b1175f5fc9ebbec011
arianabaigar2 <arianabaigar27@gmail.com>
2025-02-27 10:06:13 +0800

    Add files via upload

---
+++ cutiepwe.py
@@ -0,0 +1,12 @@
+class Public:
+    def __init__(self):
+        self.name = "cutiepwe"  
+
+    def display_name(self):
+        print(self.name)  
+
+obj = Public()
+obj.display_name()  
+print(obj.name)  
+
+        
