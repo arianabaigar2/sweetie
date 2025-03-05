3a1c36c97501c8f9e8f3c708e3a8d992d43ae16e
arianabaigar2 <arianabaigar27@gmail.com>
2025-03-06 00:05:53 +0800

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,15 @@
+class Account:
+    def __init__(self, balance):
+        self.__balance = balance 
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__balance += amount
+
+    def get_balance(self):
+        return self.__balance
+
+
+acc = Account(100)
+acc.deposit(50)
+print(acc.get_balance()) 
